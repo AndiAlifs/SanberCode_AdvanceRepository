bd5459ee45e5140ab485e3f8c4a73572f8a018a5
AndyAlyf <andyalyfsyah4@gmail.com>
2020-12-21 23:26:54 +0800

    add hapus and edit function

--- Quiz_VueJS/assets/js/app.js
+++ Quiz_VueJS/assets/js/app.js
@@ -2,14 +2,26 @@ var app = new Vue({
     el: '#app',
     data: {
         content: '',
-        hapus: 0,
-        edit: 0,
+        edit: -1,
         name: ['Muhammad iqbal mubarak', 'Ruby Prwanti', 'Faqih Muhammad']
     },
     methods: {
         addName: function() {
-            this.name.push(this.content),
-            this.content = '';
+            if (this.edit != -1){
+                this.name[this.edit] = this.content
+                this.edit = -1
+                this.content = '';
+            } else {
+                this.name.push(this.content),
+                this.content = '';
+            }
+        },
+        removeName: function (index) {
+            this.name.splice(index, 1);  
+        },
+        editName: function (index) {
+            this.content = this.name[index]
+            this.edit = index
         }
     }
 })
